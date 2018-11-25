a4f00ad3c661a3fc923d422527f2328e0a558cce
BogdanTanase <bogdan.tanasegg@yahoo.ro>
2018-11-25 19:21:47 +0200

    Update login page

--- app/src/main/java/bogdan/wifiplantfeeder/Login.java
+++ app/src/main/java/bogdan/wifiplantfeeder/Login.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
